9d16228432ce2ab50921ace54dfd78ff28337742
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2024-04-27 09:05:04 +0200

    Set Human Readable Representation for Tournament Model
    Set up a __str__ method for the tournament model to make a Readable
    representation of the tournament instance,
    As the tournament currently only contains a start date and an end date,
    the representation of the tournament is the duration of the tournament.
    (e.g: if start date is set to 2024-04-28 and end_date is set to
    2024-04-30, the __str__ method should return a string
    "2024-04-28 - 2024-04-30")
    A simple test case to assert this is included.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- tournament/models.py
+++ tournament/models.py
@@ -6,3 +6,9 @@ class Tournament(models.Model):
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        start_str = self.start_date.strftime('%Y-%m-%d')
+        end_str = self.end_date.strftime('%Y-%m-%d')
+        tournament_date = start_str + " - " + end_str
+        return tournament_date
