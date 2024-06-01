dd4d1787452e8c7684a75f7aaedffddc0797ab36
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2024-06-01 13:51:41 +0200

    List Tournaments on Index Page
    Refactor the index view to use a django generic ListView to display
    all tournaments currently in the database.
    Style is very basic for now, listing each tournament in a bullet point
    showing start and end date.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import IndexView
-
+from . import views
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("<int:tournament_id>/", views.detail, name="detail"),
 ]
--- tournament/views.py
+++ tournament/views.py
@@ -1,6 +1,20 @@
+from typing import Any
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Tournament
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Tournament
+    paginate_by = 100
     template_name = "tournament/index.html"  # Create your views here.
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+
+
+def detail(request, tournament_id):
+    return HttpResponse("You're looking at Tournament %s." % tournament_id)
