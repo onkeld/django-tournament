0de53ce7c8993f1871c8f4aa45c3eaa8c97daa9f
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2024-10-24 16:59:26 +0200

    Add Code-Style Checker "Black"
    Black is a code style checker that ensures the code to be PEP 8 standard
    conforming.
    
    Checks are run and the code is changed accordingly.
    
    Signed-off-by: Daniel Zaumsegel <daniel.zaumsegel@gmail.com>

--- static_pages/apps.py
+++ static_pages/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StaticPagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'static_pages'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "static_pages"
